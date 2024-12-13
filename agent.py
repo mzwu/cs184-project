2d242d5264553d0ffcd1d50ae32999dd89766ada
Maxwell Zen <maxwell.zen@gmail.com>
2024-12-13 13:01:09 -0500

    I forgot what I did

--- agent.py
+++ agent.py
@@ -2,7 +2,7 @@ import utils
 import time
 
 class HeuristicAgent:
-    def __init__(self, params = [-0.510066, -0.35663, -0.184483, 0.760777], trials = 3):
+    def __init__(self, params = [-0.510066, -0.35663, -0.184483, 0.760777, 0, 0], trials = 3):
         utils.initialize()
         self.params = params 
         self.trials = trials
@@ -28,12 +28,21 @@ class HeuristicAgent:
                 if state[x*10 + y] == 0:
                     holes += 1
                     holes_with_depth += x - (20 - height[y])
-        return self.params[0] * agg + self.params[1] * holes + self.params[2] * bump
+        return self.params[0] * agg + self.params[1] * holes + self.params[2] * bump + self.params[4] * agg_squared + self.params[5] * holes_with_depth
     
     def eval_action(self, state, act, height = []):
         nxt, cleared, done = utils.transition(state, act, height)
         if nxt is None:
             return -1e9
+        
+        # cur_score = -1e9
+        # height2 = utils.get_heights(nxt)
+        # for act2 in range(80):
+        #     nxt2, cleared2, done2 = utils.transition(nxt, act2, height2)
+        #     if nxt2 is None:
+        #         continue 
+        #     cur_score = max(cur_score, self.params[3] * (cleared + cleared2) + self.eval_board(nxt2))
+        # return cur_score
                 
         return self.params[3] * cleared + self.eval_board(nxt)
     
@@ -72,7 +81,23 @@ class HeuristicAgent:
     
 
 if __name__ == "__main__":
-    agent = HeuristicAgent(trials = 30)
-    print("Average 1:", agent.eval_agent())
-    agent = HeuristicAgent(params = [-0.7814559850152453, -0.023394217039222485, -0.1961356687120126, 0.591869963380358], trials = 30)
-    print("Average 2:", agent.eval_agent())
+    params1 = [-0.510066, -0.35663, -0.184483, 0.760777, 0, 0]
+    params2 = [-0.7814559850152453, -0.023394217039222485, -0.1961356687120126, 0.591869963380358, 0, 0]
+    params3 = [-0.24751747760725967, -0.05946665996584107, -0.7707773413418773, 0.1227229261690893, -0.2553315719496989, -0.5107308258403698]
+    params4 = [-0.6676757011739746, -0.31034700285664457, -0.32138862669390333, 0.0009965842688914792, -0.467225432474488, -0.36919188585391305]
+    params5 = [-0.8209688443754912, -0.24739868845190294, -0.420685070320271, 0.08634861516012744, -0.2428236403018467, -0.14631716747246387]
+
+    params_set = [params1, params2, params3, params4, params5]
+    
+    for i in range(5):
+        agent = HeuristicAgent(params = params_set[i], trials = 30)
+        print("Average " + str(i+1) + ": " + str(agent.eval_agent()))
+    # agent = HeuristicAgent(params = [-0.7814559850152453, -0.023394217039222485, -0.1961356687120126, 0.591869963380358, 0, 0], trials = 30)
+    # print("Average 2:", agent.eval_agent())
+    # agent = HeuristicAgent(params = [-0.24751747760725967, -0.05946665996584107, -0.7707773413418773, 0.1227229261690893, -0.2553315719496989, -0.5107308258403698], trials = 30)
+    # print("Average 3:", agent.eval_agent())
+
+    # agent = HeuristicAgent(params = params5, trials = 30)
+    # print("Average 5:", agent.eval_agent())
+    # agent = HeuristicAgent(params = [-0.24751747760725967, -0.05946665996584107, -0.7707773413418773, 0.1227229261690893, -0.2553315719496989, -0.5107308258403698], trials = 1)
+    # agent.eval_agent(print_flag = True)
--- spsa.py
+++ spsa.py
@@ -9,10 +9,10 @@ def normalize(p):
 
 # uses the SPSA optimizer to find the ideal parameters for the minimum eigenvalue
 # 
-def SPSA(p = [-0.510066, -0.35663, -0.184483, 0.760777], trials = 3, bounds = [(-10,0), (-10,0), (-10,0), (0,10)]):
+def SPSA(p, trials = 3, bounds = [(-10,0), (-10,0), (-10,0), (0,10), (-10,0), (-10,0)]):
 
     # set starting parameters
-    dim = 4
+    dim = len(p)
     # p=[0 for i in range(dim)]
     alpha = 0.602
     gamma = 0.101
@@ -20,7 +20,7 @@ def SPSA(p = [-0.510066, -0.35663, -0.184483, 0.760777], trials = 3, bounds = [(
     c = 0.01
     A = 100
 
-    for k in range(50):
+    for k in range(100):
         # alter parameters in the final stretch
         if k > 35:
             alpha = 1
@@ -55,4 +55,4 @@ def SPSA(p = [-0.510066, -0.35663, -0.184483, 0.760777], trials = 3, bounds = [(
     return normalize(p)
 
 if __name__ == "__main__":
-    print("Final Result:", SPSA(p = [-1, -1, -1, 1]))
+    print("Final Result:", SPSA(p = [-0.6676757011739746, -0.31034700285664457, -0.32138862669390333, 0.0009965842688914792, -0.467225432474488, -0.36919188585391305]))
